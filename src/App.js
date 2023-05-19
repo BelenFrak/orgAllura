eaef5201a3487955891bc21d57bc47d460ce1dc7
belen <belenfrak@gmail.com>
2023-05-19 08:15:14 -0300

    equipos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,87 @@
-
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/formulario/formulario';
+import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
+
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+ 
+  //Ternario --> condicion ? seMuestra : noSeMuestra
+ //o puedo usar
+  // condicion && seMuestra 
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //LISTA DE EQUIPOS
+  const equipos = [
+
+  {
+    titulo: "Programación",
+    colorPrimario: "#57C278",
+    colorSecundario:"#D9F7E9"
+  }
+  ,
+  {
+    titulo: "Front End",
+    colorPrimario: "#82CFFA",
+    colorSecundario:"#E8F8FF"
+  }
+,
+
+  {
+  titulo: "Data Science",
+  colorPrimario: "#A6D157",
+  colorSecundario:"#F0F8E2"
+  }
+,
+  {
+  titulo: "Devops",
+  colorPrimario: "#E06B69",
+  colorSecundario:"#FDE7E8"
+  }
+,
+  {
+  titulo: "Ux y Diseño",
+  colorPrimario: "#DB6EBF",
+  colorSecundario:"#FAE9F5"
+  }
+,
+  {
+  titulo: "Móvil",
+  colorPrimario: "#FFBA05",
+  colorSecundario:"#FFF5D9"
+  }
+,
+  {
+  titulo: "Innovación y Gestión",
+  colorPrimario: "#FF8A29",
+  colorSecundario:"#FFEEDF"
+  }
+
+]
+ 
   return (
     <div>
-      
-      <Header />
-      <Formulario/>
-    
+      <Header/>
+     
+      {/*{mostrarFormulario ? <Formulario /> :<></>}*/}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg  cambiarMostrar={cambiarMostrar} />
+
+        {   
+           equipos.map( (equipo) => {
+            console.log ("Equipo", equipo)
+            return <Equipo/>
+           } )
+        }
+
     </div>
-  );
-}
+    );
+  }
 
 export default App;
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,22 @@
+import { useState } from "react"
 import "./CampoTexto.css"
 
-const CampoTexto = () =>{
+const CampoTexto = (props) =>{
+   
+    const placeholderModificado = `${props.placeholder}...`
+
+    const manejarCambio = (e) => {
+        props.actualizarValor (e.target.value)
+    }
+    
     return <div className="campo-texto">
-        <label>Nombre</label>
-        <input placeholder="Ingresar nombre"/>
+        <label>{ props.titulo}</label>
+        <input 
+        placeholder={placeholderModificado} 
+        required ={props.required} 
+        value={props.valor}
+        onChange={manejarCambio}
+        />
     </div>
 }
 
